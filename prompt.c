0c87efa2b465ecd0df27d2f9e1e0f3aa6f0aaab3
Marc Jones <marc.t.jones@gmail.com>
2016-01-27 00:54:04 -0500

    adding parser using mpc

--- prompt.c
+++ prompt.c
@@ -7,9 +7,26 @@
 #include <editline/readline.h>
 #include <editline/history.h>
 
-int main (int argc, char** argv) {
+/* cc -std=c99 -Wall prompt.c -lm -ledit -o prompt */
+#include "mpc/mpc.h"
 
-  puts("Lispy version 0.0.0.0.1b");
+
+int main (int argc, char** argv) {
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Lispy    = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "							   \
+	number   : /-?[0-9]+/ ;				   \
+	operator : '+' | '-' | '*' | '/' ;		   \
+	expr	 : <number> | '(' <operator> <expr>+ ')' ; \
+	lispy	 : /^/ <operator> <expr>+ /$/ ;		   \
+    ",
+    Number, Operator, Expr, Lispy);
+
+  puts("Lispy version 0.0.0.0.2");
   puts("Press Ctrl+c to Exit\n");
 
   while (1) {
@@ -17,10 +34,19 @@ int main (int argc, char** argv) {
 
     add_history(input);
 
-    printf("No you're a %s\n", input);
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
 
     free(input);
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);  
 
   return 0;
 }
